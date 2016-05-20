cdba384d3308c21c3c320d71889d6d00d1bba615
Hugo Lima <hlglima@gmail.com>
2016-05-20 12:20:02 -0300

    some improvements

--- webpack.config.js
+++ webpack.config.js
@@ -6,14 +6,37 @@ const webpack = require('webpack');
 
 const SRC = path.resolve(__dirname, './client/src')
 const APP = path.resolve(__dirname, './client/app')
-const NODE_MODULE_PATH = path.resolve(__dirname, './node_modules')
 const SERVER_PATH = path.resolve(__dirname, './server')
+const NODE_MODULE_PATH = path.resolve(__dirname, './node_modules')
+
+let plugins = [
+  new ExtractTextPlugin('[name].[contenthash].css'),
+  new HtmlWebpackPlugin({
+    title: 'Basic React Project',
+    template: SRC + '/index_html_template.ejs'
+  })
+]
+
+if (process.env.NODE_ENV === 'production') {
+  plugins.push(
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
+    })
+  )
+}
 
 module.exports = {
   entry: SRC + '/index.js',
   output: {
     path: APP,
-    filename: 'bundle.js'
+    filename: '[name].[hash].js'
   },
   module: {
     loaders: [
@@ -31,11 +54,19 @@ module.exports = {
         loader: ExtractTextPlugin.extract('css!postcss!sass?includePaths[]=' + SRC + '&includePaths[]=' + NODE_MODULE_PATH)
       },
       {
-        test: /\.(woff2?|svg|png)$/,
+        test: /\.(jpe?g$|gif|svg|png)$/,
         loader: 'url?limit=10000'
       },
       {
-        test: /\.(eot|ttf|gif)$/,
+        test: /\.(woff2?)$/,
+        loader: 'file?mimetype=application/font-woff'
+      },
+      {
+        test: /\.(ttf)$/,
+        loader: 'file?mimetype=application/octet-stream'
+      },
+      {
+        test: /\.(eot)$/,
         loader: 'file'
       },
       {
@@ -44,23 +75,7 @@ module.exports = {
       }
     ]
   },
-  plugins: [
-    new ExtractTextPlugin('style.css'),
-    new HtmlWebpackPlugin({
-      title: 'Basic React Project',
-      template: SRC + '/index_html_template.ejs'
-    }),
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': process.env.NODE_ENV === 'production' ? JSON.stringify('production') : undefined
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      compressor: {
-        warnings: false
-      }
-    })
-  ],
+  plugins: plugins,
   postcss: [
     autoprefixer({
       browsers: ['last 2 versions']
